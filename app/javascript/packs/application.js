b52ce081f1c2579cf4973f638b3c9e5bad1d4acf
vagrant <vagrant@localhost.localdomain>
2020-05-16 03:17:33 +0000

    Foundation setting

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,12 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+require("foundation-sites")
+require("src/application")
+
+$(document).on('turbolinks:load', function() {
+  $(document).foundation()
+});
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
