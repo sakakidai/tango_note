619ee89bc16545aee5553491e15a5e833ba4dd61
sakakidai <sd.lybd1016@gmail.com>
2020-05-23 06:52:12 +0000

    change Devise paths

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  resources :word_books
-  devise_for :users, controllers: { omniauth_callbacks: "omniauth_callbacks" }
   root 'word_books#index'
+  resources :word_books
+
+  devise_for :users,
+  controllers: { omniauth_callbacks: "omniauth_callbacks" },
+  path: '/',
+  path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'signup' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: '/letter_opener'
